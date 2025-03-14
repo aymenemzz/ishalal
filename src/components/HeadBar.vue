cb645a889c4b241d6c454d7713e54efc76e7dcd2
amazouz <mzz.aymene@gmail.com>
2025-03-14 23:16:43 +0100

    ajout de la headbar v1 +  modificvation du index.html

---
+++ src/components/HeadBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="title">IsHalal</div>
+
+</template>
+
+<style scoped>
+.title {
+      color: #FFA600;
+      font-size: 5rem;
+}
+</style>
