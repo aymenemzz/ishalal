cdd30db69e9eabb0dd30a385602b57f19a312142
Toby Pollock <toby75003@gmail.com>
2025-03-15 16:31:29 +0100

    creation branch openfoodfacts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import './assets/main.css'
+//import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import Cunt from './Cunt.vue'
 
-const app = createApp(App)
-
+const app = createApp(Cunt)
 app.use(createPinia())
-
 app.mount('#app')
