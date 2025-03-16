ad710f93963e0a6bf9e3100b329d455ddc728790
Toby Pollock <toby75003@gmail.com>
2025-03-16 17:28:47 +0100

    demo openfoofacts ajouté

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-import Cunt from './Cunt.vue'
+import Cunt from './DemoOpenFoodFacts.vue'
 
 const app = createApp(Cunt)
 app.use(createPinia())
