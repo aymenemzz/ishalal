865d31e75d7dcbce23e279b317a23ed6e79c790b
amazouz <mzz.aymene@gmail.com>
2025-03-16 01:10:45 +0100

    add Input text to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 
 import HeadBar from "@/components/HeadBar.vue";
+import InputText from "@/components/HalalForm/InputText.vue";
 </script>
 
 <template>
   <HeadBar class="head-bar"/>
-  <
+  <InputText/>
 </template>
 
 <style scoped>
